c61cd49447fe14fe538312555772bbd3a7830e47
lianjz <1198315481@qq.com>
2023-05-17 23:44:36 +0800

    modify on screen and .log to differernt places

--- src/log.py
+++ src/log.py
@@ -8,52 +8,69 @@ import os
 
 class Log(object):
     '''
-封装后的logging
+        封装后的logging
     '''
-
-    def __init__(self, logger=None, log_cate='search'):
+    instance = None
+    
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls.instance:
+            cls.instance = super(Log, cls).__new__(cls)
+        return cls.instance
+    
+    def __init__(self, logger=None, log_cate='search', ):
         '''
             指定保存日志的文件路径，日志级别，以及调用文件
             将日志存入到指定的文件中
         '''
-
+        
         # 创建一个logger
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
         # 创建一个handler，用于写入日志文件
         self.log_time = time.strftime("%Y_%m_%d")
-        file_dir = os.getcwd() + '/../log/' + self.log_time
-        if not os.path.exists(file_dir):
-            os.mkdir(file_dir)
-        self.log_path = file_dir
-        self.log_name = self.log_path + "/" + log_cate + "." + self.log_time + '.log'
-        # print(self.log_name)
-
-        fh = logging.FileHandler(self.log_name, 'a')  # 追加模式  这个是python2的
-        # fh = logging.FileHandler(self.log_name, 'a', encoding='utf-8')  # 这个是python3的
-        fh.setLevel(logging.INFO)
-
+         # 定义handler的输出格式
+        self.formatter = logging.Formatter(
+            '[%(asctime)s]->[%(levelname)s]%(message)s')
+            # '[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
+            
+       
         # 再创建一个handler，用于输出到控制台
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
 
-        # 定义handler的输出格式
-        formatter = logging.Formatter(
-            '[%(asctime)s]->[%(levelname)s]%(message)s')
-            # '[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
+       
+        
+        ch.setFormatter(self.formatter)
 
         # 给logger添加handler
-        self.logger.addHandler(fh)
+        
         self.logger.addHandler(ch)
 
         #  添加下面一句，在记录日志之后移除句柄
         # self.logger.removeHandler(ch)
         # self.logger.removeHandler(fh)
         # 关闭打开的文件
-        fh.close()
+        
         ch.close()
+        
+        
 
     def getlog(self):
+        return self.logger
+    
+    def writelog(self, opt):
+        # file_dir = os.getcwd() + '/../log/' + self.log_time
+        file_dir = os.path.join(opt.results_dir, opt.experiment,'log')
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        self.log_path = file_dir
+        # self.log_name = self.log_path + "/"  + self.log_time + '.log'
+        self.log_name = self.log_path + "/"  + opt.experiment + '.log'
+        fh = logging.FileHandler(self.log_name, 'a', encoding='utf-8')  # 这个是python3的
+        fh.setLevel(logging.INFO)
+        
+        fh.setFormatter(self.formatter)
+        self.logger.addHandler(fh)
+        fh.close()
         return self.logger
